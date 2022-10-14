a725ba538089844d29b01fd18cf6c1d2940d8452
cescgrau <cesc.grau.pinyol@gmail.com>
2022-10-14 12:27:44 +0200

    rebulding Fizz_buzz revisited

--- domain/fizz_buzz_revisited/fizz_buzz_revisited.py
+++ domain/fizz_buzz_revisited/fizz_buzz_revisited.py
@@ -1,20 +1,27 @@
-from domain.fizz_buzz_revisited.fizz_buzz_tools_revisited import replacing_multiple_of
+from domain.fizz_buzz_revisited.fizz_buzz_tools_revisited import is_multiple_of
 
 
-def replacing_integers(start, end):
-    list_of_integers = []
-    for an_integer in range(start, end):
-        result = ""
-        if "3" in str(an_integer):
-            result += "fizz"
-        if "5" in str(an_integer):
-            result += "buzz"
-        if not "3" in str(an_integer) and not "5" in str(an_integer):
-            result=an_integer
-        list_of_integers.append(result)
-    return list_of_integers
+def replace_if_contains_integer(an_integer):
+    result = ""
+    if "3" in str(an_integer):
+        result += "fizz"
+    if "5" in str(an_integer):
+        result += "buzz"
+    return result
+
+
+def replace_if_contains_multiple_of(an_integer):
+    result = ""
+    if is_multiple_of(an_integer, divider=3):
+        result += "fizz"
+    if is_multiple_of(an_integer, 5):
+        result += "buzz"
+    return result
 
 
 def reaching_the_list_with_integers_and_multiples(start=1, end=101):
-    result = list(zip(replacing_multiple_of(start, end), replacing_integers(start, end)))
+    list_of_integers = []
+    for index in range(start, end):
+        list_of_integers=replace_if_contains_multiple_of(index)
+        list_of_integers= replace_if_contains_integer(index)
     return result
--- test/domain/fizz_buzz_revisited/test_fizz_buzz_revisited.py
+++ test/domain/fizz_buzz_revisited/test_fizz_buzz_revisited.py
@@ -1,19 +1,18 @@
 import unittest
 
-from domain.fizz_buzz_revisited.fizz_buzz_revisited import replacing_integers
-from domain.fizz_buzz_revisited.fizz_buzz_tools_revisited import a_returning_a_list_of_integers, replacing_multiple_of, \
-    is_multiple_of
+from domain.fizz_buzz_revisited.fizz_buzz_revisited import replace_if_contains_integer, replace_if_contains_multiple_of
+from domain.fizz_buzz_revisited.fizz_buzz_tools_revisited import a_returning_a_list_of_integers, is_multiple_of
 
 
 class TestFizzBuzz(unittest.TestCase):
     def test_contains_a_three_and_replace_for_a_Fizz(self):
-        self.assertEqual([1,2,"fizz"], replacing_integers(1,4))
+        self.assertEqual([1,2,"fizz"], replace_if_contains_integer(1, 4))
 
     def test_contains_a_five_and_replace_for_a_buzz(self):
-        self.assertEqual([4,"buzz"], replacing_integers(4,6))
+        self.assertEqual([4,"buzz"], replace_if_contains_integer(4, 6))
 
     def test_contains_an_integer(self):
-        self.assertEqual([1, 2, "fizz", 4, "buzz"], replacing_integers(1, 6))
+        self.assertEqual([1, 2, "fizz", 4, "buzz"], replace_if_contains_integer(1, 6))
 
     def test_if_a_integer_is_multiple_of_a_divider(self):
         self.assertTrue(is_multiple_of(18, 3))
@@ -26,16 +25,16 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual([1, 2, 3, 4, 5], a_returning_a_list_of_integers(start=1, end=6))
 
     def test_replacing_multiple_of_three_for_fizz(self):
-        self.assertEqual([1, 2, "fizz"], replacing_multiple_of(1, 4))
+        self.assertEqual([1, 2, "fizz"], replace_if_contains_multiple_of(1, 4))
 
     def test_replacing_multiple_of_five_for_buzz(self):
-        self.assertEqual([4, "buzz"], replacing_multiple_of(4, 6))
+        self.assertEqual([4, "buzz"], replace_if_contains_multiple_of(4, 6))
 
     def test_replacing_multiple_of_three_for_bizz_and_five_for_buzz(self):
-        self.assertEqual([1, 2, "fizz", 4, "buzz"], replacing_multiple_of(start=1, end=6))
+        self.assertEqual([1, 2, "fizz", 4, "buzz"], replace_if_contains_multiple_of(start=1, end=6))
 
     def test_replacing_multiples_of_three_and_five_adding_words_fizz_buzz(self):
-        self.assertEqual(["buzz", 11, "fizz", 13, 14, "fizzbuzz"], replacing_multiple_of(10, 16))
+        self.assertEqual(["buzz", 11, "fizz", 13, 14, "fizzbuzz"], replace_if_contains_multiple_of(10, 16))
 
 
 if __name__ == '__main__':
