5e5c33987528d596f0508f94837ef4b642c3f040
cescgrau <cesc.grau.pinyol@gmail.com>
2022-10-18 12:27:33 +0200

    delegate formatting to client

--- domain/mars_rover/nasa_transmission_destructure.py
+++ domain/mars_rover/nasa_transmission_destructure.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from domain.mars_rover.direction import Direction
 from domain.mars_rover.grid_limits import GridLimits
 from domain.mars_rover.mars_rover import Rover
@@ -11,7 +13,7 @@ def translator_destructuring(an_input: str) -> str:
     while rest:
         [rover_definition, rover_movements, *rest] = rest
         rover = build_rover(rover_definition, limits)
-        result += [calculate_rover_final_position(rover, rover_movements)]
+        result += [" ".join([str(x) for x in calculate_rover_final_position(rover, rover_movements)])]
     return "\n".join(result)
 
 
@@ -30,7 +32,7 @@ def build_rover(rover: str, limits: GridLimits) -> Rover:
     return rover
 
 
-def calculate_rover_final_position(rover: Rover, movements: str) -> str:
+def calculate_rover_final_position(rover: Rover, movements: str) -> List[int]:
     for movement in movements:
         if movement == "L":
             rover.turn_left()
@@ -38,4 +40,4 @@ def calculate_rover_final_position(rover: Rover, movements: str) -> str:
             rover.turn_right()
         elif movement == "M":
             rover.move()
-    return f"{rover.x_position} {rover.y_position} {rover.direction_view.value}"
+    return [rover.x_position, rover.y_position, rover.direction_view.value]
