64163b6dcbb9012ef98b670b77ba865c832769e3
cescgrau <cesc.grau.pinyol@gmail.com>
2022-10-18 12:39:36 +0200

    refactor first option

--- domain/mars_rover/nasa_transmission.py
+++ domain/mars_rover/nasa_transmission.py
@@ -4,10 +4,10 @@ from domain.mars_rover.mars_rover import Rover
 
 
 def translator(an_input: str) -> str:
-    items = an_input.split("\n")
-    rover_total = int((len(items)-1)/2)
-    limits = GridLimits(x=int(items[0][0]), y=int(items[0][2]))
     result = ""
+    items = an_input.split("\n")
+    rover_total = int((len(items) - 1) / 2)
+    limits = GridLimits(x=int(items[0][0]), y=int(items[0][2]))
     for rover_count in range(0, rover_total):
         rover_position_definition = rover_count * 2 + 1
         rover_movement_definition = rover_count * 2 + 2
@@ -16,12 +16,18 @@ def translator(an_input: str) -> str:
                         direction_view=Direction(items[rover_position_definition][4]),
                         limits=limits)
         movements = items[rover_movement_definition]
-        for movement in movements:
-            if movement == "L":
-                n_rover.turn_left()
-            elif movement == "R":
-                n_rover.turn_right()
-            elif movement == "M":
-                n_rover.move()
-        result += f"{n_rover.x_position} {n_rover.y_position} {n_rover.direction_view.value}\n"
+        result += calculate_final_position(n_rover, movements)
     return result.strip()
+
+
+def calculate_final_position(rover, movements):
+    result = ""
+    for movement in movements:
+        if movement == "L":
+            rover.turn_left()
+        elif movement == "R":
+            rover.turn_right()
+        elif movement == "M":
+            rover.move()
+    result += f"{rover.x_position} {rover.y_position} {rover.direction_view.value}\n"
+    return result
